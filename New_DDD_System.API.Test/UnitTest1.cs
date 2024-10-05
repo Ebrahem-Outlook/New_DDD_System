8bb7b54bdb546b3a8ea7b7cd7555733147e11646
Ebrahem-Outlook <EbrahemElbetar1@gmail.com>
2024-10-05 10:26:06 +0300

    Add project files.

---
+++ New_DDD_System.API.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace New_DDD_System.API.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ New_DDD_System.Application.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace New_DDD_System.Application.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
